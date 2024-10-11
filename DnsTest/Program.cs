d288234b85e37e47da132dd540e754bfd1e8a3db
남정욱 <wjddnr3304@gmail.com>
2024-10-11 13:45:49 +0900

    초기 커밋.

---
+++ DnsTest/Program.cs
@@ -0,0 +1,18 @@
+﻿
+//// 호스트/도메인 명에서 IP 알아내기
+
+using System.Net;
+
+/// 인터넷 호스트명 정보 얻기
+IPHostEntry hostEntry = Dns.GetHostEntry("www.naver.com");
+
+Console.WriteLine(hostEntry.HostName);
+foreach (IPAddress ip in hostEntry.AddressList)
+{
+    Console.WriteLine(ip);
+}
+
+// 로컬 호스트명 정보 얻기
+
+string hostname = Dns.GetHostName();
+IPHostEntry localhost = Dns.GetHostEntry(hostname);
